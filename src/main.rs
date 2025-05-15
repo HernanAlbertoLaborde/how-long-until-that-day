802e1219933bdd450e4a5cbe0de8ec31620d24d7
Hache <hernanlaborde@gmail.com>
2025-05-15 10:05:49 -0500

    :tada: first commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/*
+This project helps you know how many days, hours, and minutes 
+are left until a target timestamp.
+The input from console are:
+- Event Tag: Title to identify an event.
+- Target Date: Year, month and day of the event in format YYYY-MM-DD
+- Target Time: Hour, minutes and seconds of the target event in format HH-MM-SS.
+*/
+
+fn main() {
+    println!("How long until that day...");
+}
